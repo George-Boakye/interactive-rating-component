44b24ba33d67d1921797d778ce3bb184d7e8ddde
Mistok98 <georgekwamekumiboakye@gmail.com>
2022-04-01 18:46:26 +0000

    finished css of 2 cards

---
+++ main.js
@@ -0,0 +1,42 @@
+const numbers = document.querySelectorAll(".numbers");// all btns with numbers
+const btn = document.getElementById("submit");// button
+
+
+
+for (let i = 0 ; i < numbers.length; i++) {
+  numbers[i].addEventListener('click' , ()=>{
+    return document.numbers[i].outerText
+  });
+}
+
+function getSelected(){
+
+  let answer = undefined;
+
+  // const selected = numbers.addEventListener('click', () =>{
+  //  console.log( document.getElementsByClassName("numbers").value)
+  // })
+  
+
+  //   numbers.forEach(number =>{
+  //   if(number.value === sel){
+  //       console.log("marked");
+  //   }
+  // })
+
+}
+getSelected();
+
+
+
+
+
+
+  // You selected  Add rating here  out of 5
+
+  // Thank you!
+
+  // We appreciate you taking the time to give a rating. If you ever need more support, 
+  // don’t hesitate to get in touch!
+
+  
